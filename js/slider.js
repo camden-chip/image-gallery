8c35a1d8e96d65ade4721226a04878d9662bd1a2
Tatsiana Chypsanava <camden_chip@hotmail.com>
2016-04-21 10:25:57 +1200

    making next and prev buttons work

--- js/slider.js
+++ js/slider.js
@@ -8,7 +8,7 @@ for (var i = 0; i < allImages.length; i++) {
 	
 	imageSources.push( allImages[i].src );
 }
-console.log( imageSources )
+
 //delete all the images on the page
 
 var galleryContainer = document.querySelector( '#gallery');
@@ -23,19 +23,22 @@ slider.id = 'slider';
 //add the slider to the browser
 
 galleryContainer.appendChild( slider );
-var next = document.createElement('button');
+
 var prev = document.createElement('button');
+var next = document.createElement('button');
 
-next.id = 'next';
+
 prev.id = 'prev';
+next.id = 'next';
 
-next.innerHTML = "Next";
+
 prev.innerHTML = "Previous";
+next.innerHTML = "Next";
 
 //add the buttons to the slider
 
-slider.appendChild(next);
 slider.appendChild(prev);
+slider.appendChild(next);
 
 // show the first image to the visitor
  slider.style.backgroundImage = 'url('+ imageSources[0] +')';
@@ -45,18 +48,45 @@ next.onclick = changePicture;
 prev.onclick = changePicture;
 
 function changePicture() {
-	alert( this.id ); 
-}
+	
 //which image is in use?
 
 var imageInUse = slider.style.backgroundImage;
 
 // extract the URL
  var bits = imageInUse.split('"');
- console.log(bits);
 
 
+//save the link inside a variable
+var url = bits[1];
+ console.log(url);
 
+ //find out what index is in use in theimage sources
+ var index = imageSources.indexOf( url );
+
+
+//ifthe next button was clicked
+if( this.id == 'next') { 
+	index++;
+	//if the index is beyond the last image
+	if( index == imageSources.length ) { index = 0;
+		console.log('you have reached the end');
+
+	}
+	
+}else {
+		index--;
+		//if you have gone befor the firt image
+		if(index == -1) { 
+			index = imageSources. length -1;
+		}
+	}
+//get thenew URL
+var newUrl = imageSources[index];
+
+//Change the picture innthe slider
+slider.style.backgroundImage = 'url('+ newUrl +')';
+}
 
 
 
